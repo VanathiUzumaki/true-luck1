4e2ff48a063fa33a44c70f0753666cafcef667fa
Ajay <kowshik8@gmail.com>
2024-05-03 13:55:53 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start of the True Luck Code
         </p>
         <a
           className="App-link"
