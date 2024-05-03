44db75f8c7f26f721581a607e59651b44c509f26
Ajay <kowshik8@gmail.com>
2024-05-03 14:06:35 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Start of the True Luck Code
+          Start of the True Luck Code extra
         </p>
         <a
           className="App-link"
