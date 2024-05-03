d7242f019ccc5d0d4346bdeb39bc2564ae79f15d
Ajay <kowshik8@gmail.com>
2024-05-03 18:56:51 +0530

    New

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,21 @@
 import '../Styles/Navbar.css';
+import eth from '../Images/icon-eth.png';
+import drop from '../Images/icon-dropdown.png';
 
 function Navbar() {
   return (
-    <div>
-        <p>True Luck</p>
+    <div className='Nav'>
+        <div className='Logo'>
+            <p className='l'>true luck</p>
+        </div>
+        <div className='nav-elements'>
+            <p className='points'>POINTS:600</p>
+            <div className='dropdown'>
+                <img src={eth} alt='eth' className='eth'/>
+                <p>ETH</p>
+                <img src={drop} alt='drop' className='drop'/>
+            </div>
+        </div>
     </div>
   );
 }
