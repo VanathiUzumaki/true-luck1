aecc22a4198577189a6724b03ac686ed333c099f
Ajay <kowshik8@gmail.com>
2024-05-03 16:54:12 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import '../Styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <p>True Luck</p>
+    </div>
+  );
+}
+
+export default Navbar;
