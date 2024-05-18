89c02585bb7d44e84884aee394405d4bb02087c1
Ajay <kowshik8@gmail.com>
2024-05-18 13:24:26 +0530

    new

--- src/Components/Referrals.js
+++ src/Components/Referrals.js
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
 import '../Styles/Referrals.css'
+import link_img from '../Images/link.png'
 
 function Referrals(){
 
+    const [link, setLink] = useState('');
+
+    const generateLink = () => {
+        const newLink = 'https://example.com/referral?code=123456';
+        setLink(newLink);
+    }
+
     return(
         <div className='Referrals'>
+            <div className='generate-link-container'>
+                <p className='generate-text'>Generate Link:</p>
+                <input type="text" value={link} readOnly className="link-input"/>
+                <button onClick={generateLink} className="link-button">
+                    <img className='link-img' src={link_img}/>
+                </button>
+            </div>
         </div>
     );
 
