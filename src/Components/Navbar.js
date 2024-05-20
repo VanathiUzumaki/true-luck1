e3624bcc24c476e8719238fa209b6e95b40e08c2
Ajay <kowshik8@gmail.com>
2024-05-20 16:34:59 +0530

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,67 @@
+import React, { useState } from 'react';
 import '../Styles/Navbar.css';
 import eth from '../Images/icon-eth.png';
 import drop from '../Images/icon-dropdown.png';
-import profileicon from '../Images/profile-icon.png'
+import profileicon from '../Images/profile-icon.png';
+import hamburgerIcon from '../Images/hamburger-icon.png';
+import closeIcon from '../Images/close-icon.png';
 import { useNavigate } from "react-router-dom";
 
 function Navbar(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuClosing, setMenuClosing] = useState(false);
   const navigate = useNavigate();
 
   const handleConnectWallet = () => {
     console.log('Connect Wallet button clicked!');
   };
- 
+
+  const toggleMenu = () => {
+    if (menuOpen) {
+      setMenuClosing(true);
+      setTimeout(() => {
+        setMenuOpen(false);
+        setMenuClosing(false);
+      }, 300);
+    } else {
+      setMenuOpen(true);
+    }
+  };
+
   return (
     <div className='Nav'>
-        <div className='logo' onClick={() => { navigate("/");}}>true luck</div>
-        <div className='nav-elements'>
+      <div className='logo' onClick={() => { navigate("/"); }}>true luck</div>
+      <div className='nav-elements'>
+        <div className='points'>POINTS:{props.points}</div>
+        <div className='dropdown'>
+          <img src={eth} alt='eth' className='eth' />
+          <p className='dropdown-text'>ETH</p>
+          <img src={drop} alt='drop' className='drop' />
+        </div>
+        <button className='connect-wallet' onClick={handleConnectWallet}>
+          <p className='connect-wallet-text'>CONNECT WALLET</p>
+        </button>
+        <img src={profileicon} alt='profile-icon' className='profile-icon' onClick={() => { navigate("/ProfileDashboard"); }} />
+      </div>
+      <div className='hamburger' onClick={toggleMenu}>
+        <img src={menuOpen && !menuClosing ? closeIcon : hamburgerIcon} alt='menu-icon' />
+      </div>
+      {(menuOpen || menuClosing) && (
+        <div className={`mobile-menu ${menuClosing ? 'closing' : ''}`}>
+          <div className='mobile-menu-content'>
             <div className='points'>POINTS:{props.points}</div>
             <div className='dropdown'>
-                <img src={eth} alt='eth' className='eth' />
-                <p className='dropdown-text'>ETH</p>
-                <img src={drop} alt='drop' className='drop'/>
+              <img src={eth} alt='eth' className='eth' />
+              <p className='dropdown-text'>ETH</p>
+              <img src={drop} alt='drop' className='drop' />
             </div>
-            <button className='connect-wallet'  onClick={handleConnectWallet}>
+            <button className='connect-wallet' onClick={handleConnectWallet}>
               <p className='connect-wallet-text'>CONNECT WALLET</p>
             </button>
-            <img src={profileicon} alt='profile-icon' className='profile-icon'  onClick={() => { navigate("/ProfileDashboard");}} />
+            <img src={profileicon} alt='profile-icon' className='profile-icon' onClick={() => { navigate("/ProfileDashboard"); }} />
+          </div>
         </div>
+      )}
     </div>
   );
 }
