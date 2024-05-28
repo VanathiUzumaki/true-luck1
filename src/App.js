19c995aab352a91a3988a6ebcc7fb50bbf1b324b
Ajay <kowshik8@gmail.com>
2024-05-28 17:34:19 +0530

    new

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
  
   return (
     <div className="App">
-       <Router>
+       <Router basename="/true-luck">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/Home" element={<Home />} />
