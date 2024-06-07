e0d115eb583f27750cbb76203b8f0cdd6714b8f4
Ajay <kowshik8@gmail.com>
2024-06-08 03:33:51 +0530

    new

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -2,36 +2,37 @@ import React, { useState } from 'react';
 import '../Styles/Dropdown.css';
 import eth from '../Images/icon-eth.png';
 import drop from '../Images/icon-dropdown.png';
+import polygon from '../Images/polygon.png'
+import bnb from '../Images/bnb.png'
+import base from '../Images/base.png'
+import blast from '../Images/blast.svg'
 
 
-function Dropdown() {
-    const [dropdown, setDropdown] = useState(false);
-    const [selectedItem, setSelectedItem] = useState('ETH');
 
-    const toggleDropdown = () => {
-        setDropdown(!dropdown);
-      };
-    
-    const handleItemClick = (item) => {
-        setSelectedItem(item);
-        setDropdown(false);
+function Dropdown({ dropdown, selectedItem, selectedImage, toggleDropdown, handleItemClick }) {
+    const itemImageMap = {
+        'ETH': eth,
+        'Polygon': polygon,
+        'BNB' : bnb,
+        'Base' : base,
+        'Blast' : blast
     };
 
 
   return (
-    <div className={`Dropdown ${dropdown ? '' : 'menu-hidden'}`}>
+    <div className="Dropdown">
         <div className="dropdown-container">
-            <img src={eth} alt='eth' className='eth' />
+            <img src={itemImageMap[selectedImage]} alt='eth' className='eth' />
             <p className='dropdown-text'>{selectedItem}</p>
             <img src={drop} alt='drop' onClick={toggleDropdown} className={`drop ${dropdown ? 'opendropdown' : ''}`} />
         </div>
-        <ul className="menu-dropdown">
-            <li onClick={() => handleItemClick('ETH')}>ETH</li>
-            <li onClick={() => handleItemClick('Polygon')}>Polygon</li>
-            <li onClick={() => handleItemClick('BNB')}>BNB</li>
-            <li onClick={() => handleItemClick('Base')}>Base</li>
-            <li onClick={() => handleItemClick('Blast')}>Blast</li>
-        </ul>
+        <div className={`menu-dropdown ${dropdown ? 'open' : ''}`}>
+            <div onClick={() => handleItemClick('Polygon')} className='option'><img src={polygon} alt='eth' className='eth' /><span>Polygon</span></div>
+            <div onClick={() => handleItemClick('ETH')} className='option'><img src={eth} alt='eth' className='eth' /><span>ETH</span></div>
+            <div onClick={() => handleItemClick('BNB')} className='option'><img src={bnb} alt='eth' className='eth' /><span>BNB</span></div>
+            <div onClick={() => handleItemClick('Base')} className='option'><img src={base} alt='eth' className='eth' /><span>Base</span></div>
+            <div onClick={() => handleItemClick('Blast')} className='option'><img src={blast} alt='eth' className='eth' /><span>Blast</span></div>
+        </div>
     </div>
   );
 }
