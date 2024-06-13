74532817e9b7f90a1dec8c839d06cfb4f77f44e3
Priyanka Ravi <p.ravi.d@heathus.com>
2024-06-13 08:40:24 +0530

    #expanded room

--- src/Components/Expanded Room.js
+++ src/Components/Expanded Room.js
@@ -3,6 +3,7 @@ import '../Styles/Expanded Room.css';
 import expanded_room_bg from '../Images/Expanded-room-bg.png'
 import up_arrow from '../Images/Up arrow.png'
 import down_arrow from '../Images/Down arrow.png'
+import profile from '../Images/profile-pic.png'
 import CashRegisterAudio from '../Styles/Audio/cash-register-purchase.mp3';
 import Notification from "./Notification";
 
@@ -50,7 +51,16 @@ function Expanded_Room(props){
             </div>
             <div className='right-container'>
                 <div className='entry-fee'>Entry fee : $10 usdt/usdc</div>
-                <div className='participants'>Participants : 6</div>
+                <div className='winner-panel'>
+                    <div  className='winner'>WINNERS</div>
+                    <div  className='winner-1'>1ST :  <img src={profile}/> CRACKHEAD47</div>
+                    <div  className='winner-2'>2ND :  <img src={profile}/> CRACKHEAD47</div>
+                    <div  className='winner-3'>3RD :  <img src={profile}/> CRACKHEAD47</div>
+                </div>
+                <button className='claim-container' onClick={OnBuyButtonclick}>
+                        <p className='claim-text'>CLAIM</p>
+                    </button>
+                {/* <div className='participants'>Participants : 6</div>
                 <div className='tickets-remaining'>Ticket remaining : 2</div>
                 <div className='buying-section'>
                     <div className='counter'>
@@ -61,7 +71,7 @@ function Expanded_Room(props){
                     <button className='buy-container' onClick={OnBuyButtonclick}>
                         <p className='buy-text'>BUY</p>
                     </button>
-                </div>
+                </div> */}
             </div>
             {showNotification && <Notification text="A new room has been bought"/>}
         </div>
